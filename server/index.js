6aee7aeb0e8202e229525a7f2609efea60dc181c
lakshaynanda <lakshay.nanda08@gmail.com>
2021-11-09 15:13:39 -0800

    complete for deployment

--- server/index.js
+++ server/index.js
@@ -12,5 +12,11 @@ app.use(cors());
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts)
 
+if (process.env.NODE_ENV === 'productions') {
+    app.use(express.static(__dirname + '/public/'));
+
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Started on port ${port}`));
