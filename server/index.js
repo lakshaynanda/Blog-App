f971bcc2d1f697fd9dc538b2cf58a2d66328c22e
lakshaynanda <lakshay.nanda08@gmail.com>
2021-11-09 15:56:06 -0800

    changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts)
 
-if (process.env.NODE_ENV === 'productions') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'));
 
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
